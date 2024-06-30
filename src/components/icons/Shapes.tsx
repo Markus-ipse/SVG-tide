6894f57eea9e9f128d9274bb42c42cd4aab80cd0
Markus <830794+Markus-ipse@users.noreply.github.com>
2024-06-30 14:45:36 +0200

    feat: add icon for polygon shape

--- src/components/icons/Shapes.tsx
+++ src/components/icons/Shapes.tsx
@@ -39,6 +39,38 @@ const Circle = ({ fill }: IconProps) => {
   );
 };
 
+const Polygon = ({ fill }: IconProps) => {
+  const size = 16;
+
+  const numberOfSides = 5,
+    xCenter = 8,
+    yCenter = 8,
+    polygonSize = 7,
+    step = (2 * Math.PI) / numberOfSides, //Precalculate step value
+    shift = (Math.PI / 180.0) * -18; //Quick fix ;)
+
+  const points: { x: number; y: number }[] = [];
+
+  for (let i = 0; i <= numberOfSides; i++) {
+    const curStep = i * step + shift;
+    points.push({
+      x: xCenter + polygonSize * Math.cos(curStep),
+      y: yCenter + polygonSize * Math.sin(curStep),
+    });
+  }
+
+  return (
+    <svg width="1em" height="1em" viewBox={`0 0 ${size} ${size}`}>
+      <polygon
+        points={points.map((p) => `${p.x},${p.y}`).join(" ")}
+        fill={fill}
+        stroke="black"
+        stroke-width="2"
+      />
+    </svg>
+  );
+};
+
 export const ShapeIcon = ({
   shape,
   fill,
@@ -52,6 +84,8 @@ export const ShapeIcon = ({
       return <Square fill={fillValue} />;
     case "circle":
       return <Circle fill={fillValue} />;
+    case "polygon":
+      return <Polygon fill={fillValue} />;
     default:
       return "x";
   }
