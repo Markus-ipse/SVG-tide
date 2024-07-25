14c89a2fa456e983cb99bd0c596f1cb7515731ad
Markus <830794+Markus-ipse@users.noreply.github.com>
2024-07-25 23:18:09 +0200

    💅 add link to repo

--- src/App.tsx
+++ src/App.tsx
@@ -225,116 +225,141 @@ export function App() {
   ].join(" ");
 
   return (
-    <div className="flex m-8">
-      <div>
-        <h4 className="text-l font-semibold">Shapes</h4>
-        <div className="flex flex-wrap">
-          <Button
-            className="border p-1 rounded-md m-1"
-            toggled={drawingMode === "rectangle"}
-            onClick={() => setDrawingMode("rectangle")}
-          >
-            Rectangle
-          </Button>
-          <Button
-            className="border p-1 rounded-md m-1"
-            toggled={drawingMode === "circle"}
-            onClick={() => setDrawingMode("circle")}
-          >
-            Circle
-          </Button>
-        </div>
-      </div>
-      <div>
-        <svg
-          style={{ backgroundColor: "#EEE" }}
-          ref={canvasRef}
-          width={canvasSize.width}
-          height={canvasSize.height}
-          className="border-2 border-slate-200"
-          viewBox={viewBoxStr}
-          onMouseDown={(e) =>
-            drawingMode ? handleStartDrawing(e) : paz.handleMouseDown(e)
-          }
-          onMouseMove={(e) =>
-            drawingMode ? handleDrawing(e) : paz.handleMouseMove(e)
-          }
-          onMouseUp={() => (drawingMode ? stopDrawing() : paz.handleMouseUp())}
-          onMouseLeave={paz.handleMouseUp} // Handle case where mouse leaves the SVG area
-          onWheel={(e) =>
-            paz.handleZoom(
-              e.deltaY < 0,
-              e.nativeEvent.offsetX,
-              e.nativeEvent.offsetY
-            )
-          }
-          onClick={() => {
-            setSelectedElementId(null);
-          }}
+    <div>
+      <div className="flex m-8 ">
+        <h1 className="text-xl font-semibold">SVG TidE </h1>
+        <a
+          className="ml-auto"
+          href="https://github.com/Markus-ipse/SVG-tide"
+          target="_blank"
+          aria-label="view source on GitHub"
+          title="View source on GitHub"
         >
-          <rect
-            id="canvas-bg"
-            x={0}
-            y={0}
+          <svg
+            height="24"
+            aria-hidden="true"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="24"
+            data-view-component="true"
+          >
+            <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+          </svg>
+        </a>
+      </div>
+      <div className="flex m-8">
+        <div>
+          <h4 className="text-l font-semibold">Shapes</h4>
+          <div className="flex flex-wrap">
+            <Button
+              className="border p-1 rounded-md m-1"
+              toggled={drawingMode === "rectangle"}
+              onClick={() => setDrawingMode("rectangle")}
+            >
+              Rectangle
+            </Button>
+            <Button
+              className="border p-1 rounded-md m-1"
+              toggled={drawingMode === "circle"}
+              onClick={() => setDrawingMode("circle")}
+            >
+              Circle
+            </Button>
+          </div>
+        </div>
+        <div>
+          <svg
+            style={{ backgroundColor: "#EEE" }}
+            ref={canvasRef}
             width={canvasSize.width}
             height={canvasSize.height}
-            fill="#FFF"
-          />
-
-          {svgItems.toReversed().map((element) => {
-            const { type, attr } = element;
-            return (
-              <g
-                id={element.id.toString()}
-                key={element.id}
-                ref={(node) => {
-                  const map = getMap();
-                  if (node) {
-                    map.set(element, node);
-                  } else {
-                    map.delete(element);
-                  }
-                }}
-              >
-                <title>{`${type} ${element.id}`}</title>
-                {createElement(type, {
-                  onClick: (e) => {
-                    e.stopPropagation();
-                    setSelectedElementId(element.id);
-                  },
-                  key: element.id,
-                  ...attr,
-                })}
-              </g>
-            );
-          })}
-          {selectionBounds && (
-            <SelectionMarker
-              selectionBounds={selectionBounds}
-              zoomLevel={zoomLevel}
+            className="border-2 border-slate-200"
+            viewBox={viewBoxStr}
+            onMouseDown={(e) =>
+              drawingMode ? handleStartDrawing(e) : paz.handleMouseDown(e)
+            }
+            onMouseMove={(e) =>
+              drawingMode ? handleDrawing(e) : paz.handleMouseMove(e)
+            }
+            onMouseUp={() =>
+              drawingMode ? stopDrawing() : paz.handleMouseUp()
+            }
+            onMouseLeave={paz.handleMouseUp} // Handle case where mouse leaves the SVG area
+            onWheel={(e) =>
+              paz.handleZoom(
+                e.deltaY < 0,
+                e.nativeEvent.offsetX,
+                e.nativeEvent.offsetY
+              )
+            }
+            onClick={() => {
+              setSelectedElementId(null);
+            }}
+          >
+            <rect
+              id="canvas-bg"
+              x={0}
+              y={0}
+              width={canvasSize.width}
+              height={canvasSize.height}
+              fill="#FFF"
             />
-          )}
-        </svg>
-        <div className="pt-2">
-          <span className="font-semibold mr-1">Zoom:</span>
-          {Math.round(zoomLevel * 100)}%
-          <span className="font-semibold ml-4 mr-1">X/Y offset:</span>
-          {Math.round(paz.viewBox.minX)}, {Math.round(paz.viewBox.minY)}
-          <Button className="border p-1 rounded-md ml-6" onClick={paz.reset}>
-            Reset pan & zoom
-          </Button>
+
+            {svgItems.toReversed().map((element) => {
+              const { type, attr } = element;
+              return (
+                <g
+                  id={element.id.toString()}
+                  key={element.id}
+                  ref={(node) => {
+                    const map = getMap();
+                    if (node) {
+                      map.set(element, node);
+                    } else {
+                      map.delete(element);
+                    }
+                  }}
+                >
+                  <title>{`${type} ${element.id}`}</title>
+                  {createElement(type, {
+                    onClick: (e) => {
+                      e.stopPropagation();
+                      setSelectedElementId(element.id);
+                    },
+                    key: element.id,
+                    ...attr,
+                  })}
+                </g>
+              );
+            })}
+            {selectionBounds && (
+              <SelectionMarker
+                selectionBounds={selectionBounds}
+                zoomLevel={zoomLevel}
+              />
+            )}
+          </svg>
+          <div className="pt-2">
+            <span className="font-semibold mr-1">Zoom:</span>
+            {Math.round(zoomLevel * 100)}%
+            <span className="font-semibold ml-4 mr-1">X/Y offset:</span>
+            {Math.round(paz.viewBox.minX)}, {Math.round(paz.viewBox.minY)}
+            <Button className="border p-1 rounded-md ml-6" onClick={paz.reset}>
+              Reset pan & zoom
+            </Button>
+          </div>
         </div>
-      </div>
-      <div className="ml-2 w-[24rem]">
-        <ElementList
-          elements={svgItems}
-          onRemove={removeElement}
-          onReorder={reorderElement}
-          onSelect={setSelectedElementId}
-          className="mb-2"
-          selectedElementId={selectedElementId}
-        />
-        <AttributeEditor element={selectedElement} onChange={setAttribute} />
+        <div className="ml-2 w-[24rem]">
+          <ElementList
+            elements={svgItems}
+            onRemove={removeElement}
+            onReorder={reorderElement}
+            onSelect={setSelectedElementId}
+            className="mb-2"
+            selectedElementId={selectedElementId}
+          />
+          <AttributeEditor element={selectedElement} onChange={setAttribute} />
+        </div>
       </div>
     </div>
   );
